3c1c9ee2f795fc52e579c544c5e558c1a0207a31
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-02-25 13:28:10 +0300

    Refactor layout component to add custom fonts and update Clerk appearance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,26 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import {
-  ClerkProvider,
-} from '@clerk/nextjs'
-import {Inter, Space_Grotesk} from 'next/font/google'
-import { text } from "stream/consumers";
+import { ClerkProvider } from "@clerk/nextjs";
+import { Inter, Space_Grotesk } from "next/font/google";
+import { ThemeProvider } from "@/context/ThemeProvider";
 const inter = Inter({
-  subsets: ['latin'],
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-inter",
+});
 const space_Grotesk = Space_Grotesk({
-  subsets: ['latin'],
-  weight: ['300', '400', '500', '600', '700'],
-  variable: '--font-inter',
-})
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+});
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "A commity-driven platform for asking and answering programming questions. Get help, share knowledge, and collabrate with developers from around the world. Explore topics in web development, alogoithms, data structures, and more.",
+  description:
+    "A commity-driven platform for asking and answering programming questions. Get help, share knowledge, and collabrate with developers from around the world. Explore topics in web development, alogoithms, data structures, and more.",
   icons: {
     icon: "/assets/images/site-logo.svg",
-  }
+  },
 };
-
 
 export default function RootLayout({
   children,
@@ -30,22 +28,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider
-    appearance={{
-      elements: {
-        formButtonPrimary: 'primary-gradient',
-        footerActionLink: 
-        'primary-text-gradient hover:text-primary-500',
-      }
-    }}
-    >
     <html lang="en">
-      <body
-      className={`${inter.variable} ${space_Grotesk.variable}`}
-      >
-        {children}
+      <body className={`${inter.variable} ${space_Grotesk.variable}`}>
+        <ClerkProvider
+          appearance={{
+            elements: {
+              formButtonPrimary: "primary-gradient",
+              footerActionLink: "primary-text-gradient hover:text-primary-500",
+            },
+          }}
+        >
+          <ThemeProvider>{children}</ThemeProvider>
+        </ClerkProvider>
       </body>
     </html>
-    </ClerkProvider>
   );
 }
