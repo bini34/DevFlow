2fde81647e9aca286d822f2b6cb210d0cd1b2fbe
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-02-25 12:51:36 +0300

    Refactor layout component to add custom fonts and update Clerk appearance

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,17 +2,27 @@ import type { Metadata } from "next";
 import "./globals.css";
 import {
   ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton
 } from '@clerk/nextjs'
-
-
+import {Inter, Space_Grotesk} from 'next/font/google'
+import { text } from "stream/consumers";
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+})
+const space_Grotesk = Space_Grotesk({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '600', '700'],
+  variable: '--font-inter',
+})
 export const metadata: Metadata = {
   title: "DevFlow",
-  description: "DevFlow is a platform for developers to share their knowledge.",
+  description: "A commity-driven platform for asking and answering programming questions. Get help, share knowledge, and collabrate with developers from around the world. Explore topics in web development, alogoithms, data structures, and more.",
+  icons: {
+    icon: "/assets/images/site-logo.svg",
+  }
 };
+
 
 export default function RootLayout({
   children,
@@ -20,9 +30,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+    appearance={{
+      elements: {
+        formButtonPrimary: 'primary-gradient',
+        footerActionLink: 
+        'primary-text-gradient hover:text-primary-500',
+      }
+    }}
+    >
     <html lang="en">
       <body
+      className={`${inter.variable} ${space_Grotesk.variable}`}
       >
         {children}
       </body>
