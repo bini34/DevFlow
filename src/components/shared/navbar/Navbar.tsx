88ba80876f66c8dc22aecd7a3647344091b88226
Biniyam Ambachew <101281173+bini34@users.noreply.github.com>
2025-02-26 19:51:33 +0300

    Add MobileNav and GlobalSearch components to Navbar

--- src/components/shared/navbar/Navbar.tsx
+++ src/components/shared/navbar/Navbar.tsx
@@ -2,9 +2,11 @@ import Link from "next/link";
 import Image from "next/image";
 import { SignedIn, UserButton } from "@clerk/nextjs";
 import Theme from "./Theme";
+import MobileNav from "./MobileNav";
+import GlobalSearch from "../search/GlobalSearch";
 function Navbar() {
   return (
-    <nav className=" flex-between background-light900_dark200 fixed z-50 w-full gap-5 shadow-light-300 dark:shadow-none sm:px-12">
+    <nav className=" flex-between background-light900_dark200  z-50 w-full gap-5 shadow-light-300 dark:shadow-none sm:px-12">
       <Link href="/" className="flex items-center gap-1">
         <Image
           src="/assets/images/site-logo.svg"
@@ -16,6 +18,7 @@ function Navbar() {
           Dev<span className="text-primary-500">Overflow</span>
         </p>
       </Link>
+      <GlobalSearch/>
       <div className="flex-between gap-5">
       <Theme/>
         <SignedIn>
@@ -30,6 +33,7 @@ function Navbar() {
             }}
           />
         </SignedIn>
+        <MobileNav/>
       </div>
     </nav>
   );
